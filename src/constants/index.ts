c812c7da246ae54d8e1ade735448248ed7e01adb
RAFAQAT11 <kashijhony@gmail.com>
2021-10-21 20:01:13 +0500

    update new contract

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const CONTRACT_ADDRESSES = {
   LiquidityManager: '0xd918fd37c24c8f8e75408753afc1c0da91836204',
-  uniState: '0x9e19a2E7b15Aa7e272B2216ad7E9c564c2f291f7',
+  uniState: '0x52b29E16e509E673CBB8793687C92593266D3F4E',
   multiCall: '0x42ad527de7d4e9d9d011ac45b31d8551f8fe9821',
 };
 
